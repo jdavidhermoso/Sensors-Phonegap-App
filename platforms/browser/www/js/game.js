b30ca73b35c3180654df8ca9eefab882b343eb87
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-12-06 19:42:34 +0100

    Built sensor data printer

---
+++ platforms/browser/www/js/game.js
@@ -0,0 +1,37 @@
+(function () {
+    var init = function () {
+            initFastClick();
+
+            navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 1000});
+        },
+
+        initFastClick = function () {
+            FastClick.attach(document.body);
+        },
+
+        printData = function (data, element, axis) {
+            document.querySelector(element).innerHTML = axis + ': ' + roundData(data);
+        },
+
+        roundData = function (data) {
+            return Math.round(data * 100) / 100;
+        },
+
+
+        onSuccess = function (data) {
+            printData(data.x, '#value-x', 'X');
+            printData(data.y, '#value-y', 'Y');
+            printData(data.z, '#value-z', 'Z');
+        },
+
+        onError = function (err) {
+            alert(err.code + ' - ' + err.message);
+
+        };
+
+    if ('addEventListener' in document) {
+        document.addEventListener('deviceready', function () {
+            init();
+        }, false);
+    }
+})();
