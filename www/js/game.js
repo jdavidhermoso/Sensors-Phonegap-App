7fd7c8d997ba4523f190ec880e2a46a149a885d3
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-12-06 20:19:36 +0100

    Added phaser and make ball appear in screen

--- www/js/game.js
+++ www/js/game.js
@@ -1,8 +1,51 @@
 (function () {
-    var init = function () {
+    var config = {
+            BALL_DIAMETER: 50,
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        },
+        init = function () {
             initFastClick();
+            watchSensors();
+            startGame();
+        },
+
+        startGame = function () {
+            function preload() {
+                game.stage.backgroundColor = '#f27d0c';
+                game.load.image('ball', 'img/ballsm.png');
+            }
+
+            function create() {
+                game.add.sprite(initX(), initY(), 'ball');
+            }
+
+            var states = {preload: preload, create: create};
+            var game = new Phaser.Game(config.width, config.height, Phaser.CANVAS, 'phaser', states);
+        },
+
+        initX = function () {
+            return getRandomNumber(config.width - config.BALL_DIAMETER);
+        },
+
+        initY = function () {
+            return getRandomNumber(config.height - config.BALL_DIAMETER);
+        },
+
+        watchSensors = function () {
+            function onSuccess(data) {
+                detectShaking(data);
+            }
+
+            function onError(err) {
+                alert(err.code + ' - ' + err.message);
+            }
 
             navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 1000});
+        },
+
+        getRandomNumber = function (max) {
+            return Math.floor(Math.random() * max);
         },
 
         initFastClick = function () {
@@ -17,10 +60,6 @@
             return Math.round(data * 100) / 100;
         },
 
-        onSuccess = function (data) {
-            detectShaking(data);
-            showCurrentData(data);
-        },
 
         detectShaking = function (data) {
             document.body.className = data.x > 10 || data.y > 10 ? 'shaking' : '';
@@ -30,11 +69,6 @@
             printData(data.x, '#value-x', 'X');
             printData(data.y, '#value-y', 'Y');
             printData(data.z, '#value-z', 'Z');
-        },
-
-        onError = function (err) {
-            alert(err.code + ' - ' + err.message);
-
         };
 
     if ('addEventListener' in document) {
