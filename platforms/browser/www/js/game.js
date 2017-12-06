a897fac24fa01cc08841e4cf3420168cde2937f8
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-12-06 20:50:39 +0100

    Now, ball is moving according to accelerometer changes

--- platforms/browser/www/js/game.js
+++ platforms/browser/www/js/game.js
@@ -1,6 +1,8 @@
 (function () {
     var config = {
             BALL_DIAMETER: 50,
+            speedX: 0,
+            speedY: 0,
             width: document.documentElement.clientWidth,
             height: document.documentElement.clientHeight
         },
@@ -12,15 +14,23 @@
 
         startGame = function () {
             function preload() {
+                game.physics.startSystem(Phaser.Physics.ARCADE);
+
                 game.stage.backgroundColor = '#f27d0c';
                 game.load.image('ball', 'img/ballsm.png');
             }
 
             function create() {
-                game.add.sprite(initX(), initY(), 'ball');
+                ball = game.add.sprite(initX(), initY(), 'ball');
+                game.physics.arcade.enable(ball);
             }
 
-            var states = {preload: preload, create: create};
+            function update() {
+                ball.body.velocity.y = (config.speedY * 40);
+                ball.body.velocity.x = (config.speedX * -40clear);
+            }
+
+            var states = {preload: preload, create: create, update: update};
             var game = new Phaser.Game(config.width, config.height, Phaser.CANVAS, 'phaser', states);
         },
 
@@ -34,19 +44,25 @@
 
         watchSensors = function () {
             function onSuccess(data) {
-                detectShaking(data);
+                registerDirection(data);
+                showCurrentData(data);
             }
 
             function onError(err) {
                 alert(err.code + ' - ' + err.message);
             }
 
-            navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 1000});
+            navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 10});
         },
 
-        getRandomNumber = function (max) {
-            return Math.floor(Math.random() * max);
-        },
+        registerDirection = function (data) {
+            config.speedX = data.x;
+            config.speedY = data.y;
+        };
+
+    getRandomNumber = function (max) {
+        return Math.floor(Math.random() * max);
+    },
 
         initFastClick = function () {
             FastClick.attach(document.body);
@@ -55,20 +71,29 @@
         printData = function (data, element, axis) {
             document.querySelector(element).innerHTML = axis + ': ' + roundData(data);
         },
-
+        detectShaking = function (data) {
+            document.body.className = data.x > 10 || data.y > 10 ? 'shaking' : '';
+            if (data.x > 10 || data.y > 10) {
+                setTimeout(restart, 1000);
+            }
+        },
         roundData = function (data) {
             return Math.round(data * 100) / 100;
         },
-
-
-        detectShaking = function (data) {
-            document.body.className = data.x > 10 || data.y > 10 ? 'shaking' : '';
+        restart = function () {
+            document.location.reload(true);
         },
 
         showCurrentData = function (data) {
             printData(data.x, '#value-x', 'X');
             printData(data.y, '#value-y', 'Y');
             printData(data.z, '#value-z', 'Z');
+            printSpeed('x', config.speedX);
+            printSpeed('y', config.speedY);
+        },
+
+        printSpeed = function (axis, speed) {
+            document.querySelector('#value-speed-' + axis).innerHTML = roundData(speed);
         };
 
     if ('addEventListener' in document) {
